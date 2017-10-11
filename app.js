7c5a62a86d884087f845fd09fcb585befb99548e
orochitian <orochitian@126.com>
2017-10-11 17:31:59 +0800

    网站静态资源

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'html');
+
+require('nunjucks').configure('./view', {
+    autoescape : true,
+    express : app,
+    watch : true
+});
+
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/', function (req, res) {
+    res.render('index.html');
+});
+
+app.use('*', function (req, res) {
+    res.send('<h3>页面没找到，你说尴尬不尴尬？</h3>');
+});
+
+app.listen('80', function() {
+    console.log('服务器运行中...')
+});
