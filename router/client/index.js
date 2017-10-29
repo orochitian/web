f5ec06fead9720ceb7c6003d7dd5a57a3ca2c7c8
凡生 <orochitian@126.com>
2017-10-29 23:32:58 +0800

    解决promise问题

--- router/client/index.js
+++ router/client/index.js
@@ -9,18 +9,19 @@ router.use(function (req, res, next) {
 });
 
 router.get('/', function (req, res) {
-    var promise = Welcome.findOne().then(function (info) {
-        req.pageInfo.welcome = info;
-    });
-    promise = promise.then(function () {
+    Promise.all([
+        Welcome.findOne().then(function (info) {
+            req.pageInfo.welcome = info;
+        }),
         storyCategory.find().then(function (info) {
             req.pageInfo.story = info;
-        });
+        }),
+        slider.find({category:'index'}).then(function (info) {
+            req.pageInfo.slider = info;
+        })
+    ]).then(function () {
+        res.render('index.html', req.pageInfo);
     });
-    slider.find({category:'index'}).then(function (info) {
-        req.pageInfo.slider = info;
-    });
-    res.render('index.html', req.pageInfo);
 });
 
 
